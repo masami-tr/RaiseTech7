b8ded24e1191f2919252d347fbe21b3aa54a3b71
masami-tr <m3o.mmmnnn@gmail.com>
2023-08-30 20:11:34 +0900

    クエリ文字列を受け取れるように変更

--- src/main/java/com/raizetech7/demo/NameController.java
+++ src/main/java/com/raizetech7/demo/NameController.java
@@ -5,21 +5,20 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
+//import java.util.List;
 import java.util.Map;
 
-import static java.lang.constant.ClassDesc.of;
+//import static java.lang.constant.ClassDesc.of;
 
 @RestController
 public class NameController{
 
-  @GetMapping("/names/{id}")
-  public Name getNames(@PathVariable int id){
+//クエリ文字列を受け取れるように変更
+  @GetMapping("/names")
+  public Name getNames(@RequestParam int id){
 
     return new Name(id,"masami","teramoto");
   }
-
-
 
 
   @PostMapping("/names")
